88988e69c4f836240dcfecbf712bc176880025d2
unknown <>
2017-04-21 22:58:28 +0800

    '实现左边的菜单选项切换'

--- app/app.js
+++ app/app.js
@@ -1,13 +1,18 @@
 (function(angular) {
-
-    'use strict';
-
     // Declare app level module which depends on views, and components
-    angular.module('myApp', [
-        'ngRoute',
-        'movie_list',
-    ]).config(['$routeProvider', function($routeProvider) {
+    angular.module('myApp', ['ngRoute','movie_list']).config(['$routeProvider', function($routeProvider) {
         $routeProvider.otherwise({ redirectTo: '/in_theaters/1' });
+    }]).controller('myController', ['$scope', '$location', function($scope, $location) {
+        $scope.location = $location;
+        $scope.$watch('location.path()', function(now) {
+            if (now.startsWith('/in_theaters')) {
+                $scope.type = 'in_theaters';
+            } else if (now.startsWith('/coming_soon')) {
+                $scope.type = 'coming_soon';
+            } else if (now.startsWith('/top250')) {
+                $scope.type = 'top250';
+            }
+        })
     }]);
 
 })(angular)
