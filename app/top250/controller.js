9d9e46ae1e6fa805b99765673949c0f41b1cd537
unknown <>
2017-04-21 15:22:51 +0800

    '将所有的数据都用jsonp的方式从豆瓣上取'

--- app/top250/controller.js
+++ app/top250/controller.js
@@ -1,7 +1,7 @@
 (function(angular) {
     'use strict';
 
-    var module = angular.module('moviecat.top250', ['ngRoute']);
+    var module = angular.module('moviecat.top250', ['ngRoute', 'moviecat.services.http']);
 
     module.config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/top250', {
@@ -10,14 +10,13 @@
         });
     }]);
 
-    module.controller('top250Controller', ['$scope', '$http', function($scope, $http) {
-        $http.get('/app/top.json').then(
-            function(data) {
-                $scope.dataList = data.data.subjects;
-            },
-            function(err) {
-                console.log(err);
-            });
+    module.controller('top250Controller', ['$scope', 'HttpService', function($scope, HttpService) {
+        $scope.dataList = [];
+        $scope.message = '';
+        HttpService.jsonp('https://api.douban.com/v2/movie/top250', {}, function(data) {
+            $scope.dataList = data.subjects;
+            $scope.$apply('dataList');
+        })
     }]);
 
 })(angular)
